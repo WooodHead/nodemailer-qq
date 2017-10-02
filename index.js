0bf44d7a1657a8af79f8a070f2e3eac2e8ac9184
WooodHead <WooodHead@users.noreply.github.com>
2017-10-02 18:16:05 +0800

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+var nodemailer = require("nodemailer");
+var smtpTransport = require('nodemailer-smtp-transport');
+var wellknown = require("nodemailer-wellknown");
+
+// var config = wellknown("QQ");
+// config.auth = {
+//   user: 'username',
+//   pass: 'password'
+// }
+var config = require('./config.js');
+
+// var mailOptions = {
+//   from: "aa@bb.com",
+//   to: "cc@dd.com",
+//   subject: "test",
+//   text: "text plain",
+//   html: "<div>div content</div>"
+// };
+
+var mailOptions = require('./mail-options.js')
+
+var transporter = nodemailer.createTransport(smtpTransport(config));
+
+// send mail with defined transport object
+transporter.sendMail(mailOptions, function (error, info) {
+  if (error) {
+    return console.log(error);
+  }
+  console.log('Message sent: ' + info.response);
+
+});
